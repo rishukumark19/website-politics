fc344fb12beec0f25e7a718d33e6b16df6feefea
Rishu Kumar <rishukumar.spam@gmail.com>
2025-12-03 11:54:25 +0530

    updated

--- constants.js
+++ constants.js
@@ -222,7 +222,7 @@ window.content = {
       socialLinks: [
         {
           name: "Facebook",
-          href: "https://facebook.com/RaginiSingh.official/",
+          href: "https://www.facebook.com/raginisingh01",
           path: "M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z",
         },
         {
@@ -497,7 +497,7 @@ window.content = {
       socialLinks: [
         {
           name: "Facebook",
-          href: "https://facebook.com/RaginiSingh.official/",
+          href: "https://www.facebook.com/raginisingh01",
           path: "M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z",
         },
         {
