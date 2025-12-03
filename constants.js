a219fd67872bc5c0bab2868ebfa74675f3a64632
Rishu Kumar <rishukumar.spam@gmail.com>
2025-12-03 16:31:39 +0530

    updated

--- constants.js
+++ constants.js
@@ -227,7 +227,7 @@ window.content = {
         },
         {
           name: "Twitter",
-          href: "https://instagram.com/RaginiSingh.official/",
+          href: "https://x.com/raginisingh7007",
           path: "M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z",
         },
         {
@@ -247,7 +247,7 @@ window.content = {
       formMessageLabel: "Your Message",
       formSubmitLabel: "Submit",
     },
-  
+
     journey: {
       title: "Political Journey",
       events: [
@@ -519,7 +519,7 @@ window.content = {
       formMessageLabel: "आपका संदेश",
       formSubmitLabel: "प्रस्तुत करें",
     },
-    
+
     journey: {
       title: "राजनीतिक सफ़र",
       events: [
